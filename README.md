ac071e1bb33bfca49fff7567a5219317857c679b
Lucas Hayashi <lucashayashi5@gmail.com>
2023-01-26 20:31:30 -0300

    feat: Adicionado favicon, e README.md com informações do projeto, e refatoração do código CSS

---
+++ README.md
@@ -0,0 +1,9 @@
+# Z-commerce - Integração de E-commerce
+
+## Landing page para prática do Flexbox
+
+A ideia do Zcommerce é realizar integrações entre e-commerces para facilitar o gerenciamento das vendas.
+
+Também foi criado um wireframe no Figma para me ajudar no desenvolvimento.
+
+[Visualizar wireframe](https://www.figma.com/file/L4m1UC8WryQ2D9GjovNwZz/wcommerce---home?node-id=0%3A1&t=KytLoITI308wjSLH-1)
--- js/script.js
+++ js/script.js
@@ -1,52 +1,54 @@
-let btnMenu = document.querySelector('.act-menu');
-let btnEnviar = document.getElementById('btn-enviar');
+let btnMenu = document.querySelector(".act-menu");
+let btnEnviar = document.getElementById("btn-enviar");
 
-function alternarClasse(element, className){
+function alternarClasse(element, className) {
     element.classList.toggle(className);
 }
 
-function alternarBtnMenu(mobileMenu, classValidity){
-    let menuIcon = document.querySelector('#menu-control');
-    if ( mobileMenu.classList.contains(classValidity) ){
-        menuIcon.innerText = 'close';
-    }else {
-        menuIcon.innerText = 'menu';
+function alternarBtnMenu(mobileMenu, classValidity) {
+    let menuIcon = document.querySelector("#menu-control");
+    if (mobileMenu.classList.contains(classValidity)) {
+        menuIcon.innerText = "close";
+    } else {
+        menuIcon.innerText = "menu";
     }
 }
 
-function mostrarMenu(e){
-    let mobileMenu = document.querySelector('.mobile-menu');
-    alternarClasse(mobileMenu, 'active');
-    alternarBtnMenu(mobileMenu, 'active');
+function mostrarMenu(e) {
+    let mobileMenu = document.querySelector(".mobile-menu");
+    alternarClasse(mobileMenu, "active");
+    alternarBtnMenu(mobileMenu, "active");
 }
 
-function animaSection(){
-    let sections = document.querySelectorAll('.scroll-section');
-    let elementsList = document.querySelectorAll('.image-tag, .tag-title, .text-tag');
+function animaSection() {
+    let sections = document.querySelectorAll(".scroll-section");
+    let elementsList = document.querySelectorAll(
+        ".image-tag, .tag-title, .text-tag"
+    );
 
     sections.forEach((section) => {
         let screenCalc = window.innerHeight * 0.7;
         let topDist = section.getBoundingClientRect().top - screenCalc;
-        if (topDist < 0){
-            section.classList.add('scroll-ativo');
-                if (section.id == "produto"){
-                    elementsList.forEach((item)=> {
-                        item.classList.add('on');
-                    });
-                }
-        }else {
-            section.classList.remove('scroll-ativo');
-            if (section.id == "produto"){
-                elementsList.forEach((item)=> {
-                    item.classList.remove('on');
+        if (topDist < 0) {
+            section.classList.add("scroll-ativo");
+            if (section.id == "produto") {
+                elementsList.forEach((item) => {
+                    item.classList.add("on");
+                });
+            }
+        } else {
+            section.classList.remove("scroll-ativo");
+            if (section.id == "produto") {
+                elementsList.forEach((item) => {
+                    item.classList.remove("on");
                 });
             }
         }
-    })
+    });
 }
 
 animaSection();
 
-window.addEventListener('scroll',animaSection);
+window.addEventListener("scroll", animaSection);
 
-btnMenu.addEventListener('click',mostrarMenu);
+btnMenu.addEventListener("click", mostrarMenu);
